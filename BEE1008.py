c1d71ee8dfa8b7554a09f8c3dc5b52a219bf4a41
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-12-20 19:32:34 +0600

    Add files via upload

---
+++ BEE1008.py
@@ -0,0 +1,7 @@
+NUMBER = int(input())
+work_hour_month = int(input())
+salary_hour = float(input())
+SALARY =(( salary_hour )*( work_hour_month ))
+
+print('NUMBER =',NUMBER)
+print('SALARY = U$ %0.2f' %SALARY)
